ef65ba8900e615715fae05d85473e6790bb38b1f
Matthew Stadelman <stadelmanma@gmail.com>
2020-06-21 09:12:01 -0400

    Implement elevation data importing for record messages

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use fitparser::profile::MesgNum;
 use fitparser::{FitDataRecord, Value};
 use log::{debug, error, trace};
 use rusqlite::types::ToSqlOutput;
-use rusqlite::{params, ToSql};
+use rusqlite::{NO_PARAMS, params, ToSql};
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
 use std::fmt;
@@ -11,21 +11,27 @@ use std::io::prelude::*;
 use std::iter::FromIterator;
 use std::ops::Deref;
 
+mod elevation;
+pub use elevation::{Location, request_elevation_data};
 mod schema;
 pub use schema::{create_database, open_db_connection};
 
 /// General error type for the crate
 #[derive(Debug)]
 enum Error {
+    ArrayConversionError,
     DuplicateFileError,
-    ArrayConversionError
+    ElevationRequestError(reqwest::StatusCode, String),
+    FileDoesNotExistError(String),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            Error::ArrayConversionError => write!(f, "Cannot convert Value:Array into a SQL parameter"),
             Error::DuplicateFileError => write!(f, "Attempted to import a file already in the database"),
-            Error::ArrayConversionError => write!(f, "Cannot convert Value:Array into a SQL parameter")
+            Error::ElevationRequestError(code, msg) => write!(f, "Elevation data request failed with code: {} - {}", code, msg),
+            Error::FileDoesNotExistError(uuid) => write!(f, "FIT File with UUID='{}' does not exist", uuid),
         }
     }
 }
@@ -85,7 +91,7 @@ impl ToSql for SqlValue<'_> {
 }
 
 /// Import raw fit file data into the local database
-pub fn import_fit_data<T: Read>(fp: &mut T) -> Result<(), Box<dyn std::error::Error>> {
+pub fn import_fit_data<T: Read>(fp: &mut T) -> Result<String, Box<dyn std::error::Error>> {
     let mut data = Vec::new();
     fp.read_to_end(&mut data)?;
 
@@ -192,19 +198,16 @@ pub fn import_fit_data<T: Read>(fp: &mut T) -> Result<(), Box<dyn std::error::Er
             _ => trace!("Skipped {} message with data: {:?}", mesg.kind(), data),
         }
     }
-
+    // commit transaction to store data imported from file and then fetch elevation data
     tx.commit()?;
-    Ok(())
+    if let Err(e) = update_elevation_data(Some(&uuid)) {
+        error!("Could not add in elevation data from the API for file with UUID='{}'", uuid);
+        error!("{}", e)
+    }
+
+    Ok(uuid)
 }
 
-/// Build a hash map of field references that can be acessed by field name
-pub fn create_fit_data_map<'a>(mesg: &'a FitDataRecord) -> HashMap<&'a str, SqlValue> {
-    HashMap::from_iter(
-        mesg.fields()
-            .iter()
-            .map(|f| (f.name(), SqlValue::new(f.value()))),
-    )
-}
 
 /// Create a UUID by taking the SHA256 hash of the data and then converting it to UUID4 format
 fn generate_uuid(data: &[u8]) -> String {
@@ -226,4 +229,70 @@ fn generate_uuid(data: &[u8]) -> String {
     uuid.insert(8, '-');
 
     uuid
+}
+
+/// Build a hash map of field references that can be acessed by field name
+pub fn create_fit_data_map<'a>(mesg: &'a FitDataRecord) -> HashMap<&'a str, SqlValue> {
+    HashMap::from_iter(
+        mesg.fields()
+            .iter()
+            .map(|f| (f.name(), SqlValue::new(f.value()))),
+    )
+}
+
+/// Update elevation for a FIT file or across all data in the database
+pub fn update_elevation_data(uuid: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = open_db_connection()?;
+    let tx = conn.transaction()?;
+
+    // first add elevation data for the record messages
+    match uuid {
+        Some(uuid) => {
+            if let Ok(id) = tx.query_row("select id from files where uuid = ?", params![uuid], |r| r.get::<usize, i32>(0)) {
+                let mut stmt = tx.prepare("select position_lat, position_long, id from record_messages where
+                                             file_id = ? and
+                                             position_lat is not null and
+                                             position_long is not null")?;
+                let rows = stmt.query(params![id])?;
+                request_and_update_elevation_data(&tx, "record_messages", rows)?;
+            }
+            else {
+                error!("FIT File with UUID='{}' does not exist", uuid);
+                return Err(Box::new(Error::FileDoesNotExistError(uuid.to_string())));
+            }
+        },
+        None => {
+            let mut stmt = tx.prepare("select position_lat, position_long, id from record_messages where
+                                         position_lat is not null and
+                                         position_long is not null and
+                                         elevation is null")?;
+            let rows = stmt.query(NO_PARAMS)?;
+            request_and_update_elevation_data(&tx, "record_messages", rows)?;
+        }
+    };
+
+    // todo lap messages
+
+    tx.commit()?;
+    Ok(())
+}
+
+/// Updates a set of rows with elevation data by querying the elevation API and then passing that
+/// data back into the database
+fn request_and_update_elevation_data(tx: &rusqlite::Transaction, table: &'static str, mut rows: rusqlite::Rows) -> Result<(), Box<dyn std::error::Error>> {
+    let mut locations: Vec<Location> = Vec::new();
+    let mut record_ids: Vec<i32> = Vec::new();
+    while let Some(row) = rows.next()? {
+        locations.push(Location::from_fit_coordinates(row.get(0)?, row.get(1)?));
+        record_ids.push(row.get(2)?);
+    }
+    request_elevation_data(&mut locations)?;
+
+    let stmt = format!("update {} set elevation = ? where id = ?", table);
+    let mut stmt = tx.prepare_cached(&stmt)?;
+    for (loc, rec_id) in locations.iter().zip(record_ids) {
+        stmt.execute(params![loc.elevation().map(|v| v as f64), rec_id])?;
+    }
+
+    Ok(())
 }
