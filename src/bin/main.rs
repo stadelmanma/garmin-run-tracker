583625a4e8f96f5d29688a45770be987f05beda6
Matthew Stadelman <stadelmanma@gmail.com>
2020-06-22 20:15:49 -0400

    Handle duplicates better and allow them to be ignored

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,6 @@
-use garmin_run_tracker::{create_database, import_fit_data, update_elevation_data};
 use garmin_run_tracker::elevation::OpenTopoData;
-use log::{info, trace, error};
+use garmin_run_tracker::{create_database, import_fit_data, update_elevation_data, Error};
+use log::{error, info, trace, warn};
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use std::fs::File;
 use std::path::PathBuf;
@@ -16,6 +16,9 @@ struct Cli {
     /// A level of verbosity, and can be used up to three times for maximum logging (e.g. -vvv)
     #[structopt(short, long, parse(from_occurrences))]
     verbose: i32,
+    /// Silently ignore duplicate files and emit no messages
+    #[structopt(long)]
+    ignore_duplicate_files: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -37,17 +40,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // use a locally hosted instance of opentopodata as the elevation data source
     // we have it configured to use the ned10m dataset and a max request batch size of
     // 100.
-    let topo = OpenTopoData::new("http://localhost:5000".to_string(), "ned10m".to_string(), 100);
+    let topo = OpenTopoData::new(
+        "http://localhost:5000".to_string(),
+        "ned10m".to_string(),
+        100,
+    );
 
     // Import each fit file
+    let num_files = opt.files.len();
     for file in opt.files {
         trace!("Importing FIT file: {:?}", file);
         let mut fp = File::open(&file)?;
-        let uuid = import_fit_data(&mut fp)?;
-        info!("Successfully imported FIT file: {:?} (UUID={})", file, uuid);
+        let uuid = match import_fit_data(&mut fp) {
+            Ok(uuid) => uuid,
+            Err(e) => {
+                // handle various errors
+                match &e {
+                    Error::DuplicateFileError(_) => {
+                        if opt.ignore_duplicate_files {
+                            trace!("{}", e);
+                            continue;
+                        } else if num_files == 1 {
+                            // if we are importing a single file and it's a dupe throw a hard error
+                            error!("{}", e);
+                            return Err(Box::new(e));
+                        } else {
+                            // if we are impoting multiple files, just warn about the dupe
+                            warn!("{}", e);
+                            continue;
+                        }
+                    }
+                    _ => return Err(Box::new(e)),
+                }
+            }
+        };
+
+        info!(
+            "Successfully imported FIT file: {:?} (UUID={})",
+            &file, &uuid
+        );
         if let Err(e) = update_elevation_data(&topo, &uuid) {
-            error!("Could not import elevation data from the API for FIT file with UUID='{}'", uuid);
+            error!(
+                "Could not import elevation data from the API for FIT file '{}'",
+                &uuid
+            );
             error!("{}", e)
+        } else {
+            info!("Successfully imported elevation for FIT file '{}'", &uuid);
         }
     }
 
