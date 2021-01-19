2d286645c0dabbd334b59d02d56081058def4b5a
Matthew Stadelman <stadelmanma@gmail.com>
2021-01-18 21:00:54 -0500

    Change structure of plotting to better handle multiple plots and series

--- src/services/visualization/plotting/mod.rs
+++ src/services/visualization/plotting/mod.rs
@@ -4,21 +4,39 @@ use crate::Error;
 mod tui;
 pub use self::tui::TerminalPlotter;
 
-/// alias for a vector of (x, y) coordinate pairs
-pub type DataSeries<'a> = &'a [(f64, f64)];
+/// A vector of (x, y) coordinate pairs and a name
+#[derive(Debug)]
+pub struct DataSeries<'a> {
+    name: &'a str,
+    data: &'a [(f64, f64)],
+}
+
+impl<'a> DataSeries<'a> {
+    pub fn new(name: &'a str, data: &'a [(f64, f64)]) -> Self {
+        DataSeries { name, data }
+    }
+
+    pub fn name(&self) -> &'a str {
+        self.name
+    }
+
+    pub fn data(&self) -> &'a [(f64, f64)] {
+        self.data
+    }
+}
 
 /// Defines the labels applied to the plot
 #[derive(Debug)]
-pub struct PlotLabels {
+pub struct Plot<'a> {
     title: String,
     x_axis: String,
     y_axis: String,
-    series: Vec<String>,
+    series: Vec<DataSeries<'a>>,
 }
 
-impl PlotLabels {
+impl<'a> Plot<'a> {
     pub fn new(title: String, x_axis: String, y_axis: String) -> Self {
-        PlotLabels {
+        Plot {
             series: Vec::new(),
             x_axis,
             y_axis,
@@ -38,23 +56,19 @@ impl PlotLabels {
         &self.y_axis
     }
 
-    pub fn series(&self) -> &[String] {
+    pub fn series(&self) -> &[DataSeries<'a>] {
         &self.series
     }
 
-    pub fn add_series_label(&mut self, label: String) {
-        self.series.push(label);
+    pub fn add_series(&mut self, data: DataSeries<'a>) {
+        self.series.push(data);
     }
 }
 
 /// trait that defines how to plot a set of data series
 pub trait DataPlottingService {
     /// Draw a plot of data to display to the user
-    fn plot(
-        &self,
-        series: &[DataSeries],
-        labels: &PlotLabels,
-    ) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
+    fn plot(&self, plots: &[&Plot]) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
 }
 
 pub fn new_plotting_visualization_handler(
