e26d4080b9e0370256ce1a003a45b9a2dbb69d9d
Matthew Stadelman <stadelmanma@gmail.com>
2021-01-31 07:54:47 -0500

    Initial split of project into a main crate and a derive crate

---
+++ garmin_run_tracker_derive/src/config.rs
@@ -0,0 +1,5 @@
+
+pub fn expand_derive_from_service_config(
+    input: &mut syn::DeriveInput,
+) -> Result<TokenStream, Vec<syn::Error>> {
+}
---
+++ garmin_run_tracker_derive/src/lib.rs
@@ -0,0 +1,25 @@
+//! Define procedural macro to process service config entries
+#[macro_use]
+extern crate quote;
+#[macro_use]
+extern crate syn;
+extern crate proc_macro;
+extern crate proc_macro2;
+
+use proc_macro::TokenStream;
+use syn::DeriveInput;
+
+mod config
+
+#[proc_macro_derive(FromServiceConfig)]
+pub fn derive_from_service_config(input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as DeriveInput);
+    config::expand_derive_from_service_config(&mut input)
+        .unwrap_or_else(to_compile_errors)
+        .into()
+}
+
+fn to_compile_errors(errors: Vec<syn::Error>) -> proc_macro2::TokenStream {
+    let compile_errors = errors.iter().map(syn::Error::to_compile_error);
+    quote!(#(#compile_errors)*)
+}
