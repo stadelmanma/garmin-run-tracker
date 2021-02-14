1667591238ede24a03b0740242be06552c5e3e9e
Matthew Stadelman <stadelmanma@gmail.com>
2021-02-13 22:26:15 -0500

    Cleanup update-elevation subcommand and implement --list-missing opt

--- garmin_run_tracker/src/cli/update_elevation.rs
+++ garmin_run_tracker/src/cli/update_elevation.rs
@@ -1,8 +1,10 @@
 //! Define FIT file update-elevation command
 use crate::config::Config;
 use crate::db::{find_file_by_uuid, open_db_connection};
-use crate::services::update_elevation_data;
+use crate::services::{update_elevation_data, ElevationDataSource};
 use log::{error, info};
+use rusqlite::{params, Connection};
+use std::collections::HashSet;
 use structopt::StructOpt;
 
 /// Update elevation data for one or more FIT files, all entries with missing elevation data
@@ -29,6 +31,13 @@ pub fn update_elevation_command(
     config: Config,
     opts: UpdateElevationOpts,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = open_db_connection()?;
+
+    // return UUIDs of files with missing elevation data but valid lat/long points
+    if opts.list_missing {
+        return list_missing(&conn);
+    }
+
     // fetch elevation service from config
     let elevation_hdl = match config.get_elevation_handler() {
         Ok(hdl) => hdl,
@@ -37,49 +46,12 @@ pub fn update_elevation_command(
             return Err(Box::new(e));
         }
     };
-    let mut conn = open_db_connection()?;
-
-    // return UUIDs of files with missing elevation data but valid lat/long points
-    if opts.list_missing {
-        unimplemented!();
-        return Ok(());
-    }
 
     // update elevation data for specified files, we handle each file in it's own transaction
     // so that not everything gets rolled back if it fails. API calls may not be free so we don't
     // want to waste them if possible.
     for uuid in opts.uuids {
-        // locate file_id from uuid
-        let file_info = match find_file_by_uuid(&conn, &uuid) {
-            Ok(info) => info,
-            Err(e) => return Err(Box::new(e)),
-        };
-        if file_info.id().is_none() {
-            error!(
-                "File with UUID={} has no file_id cannot update elevation data.",
-                file_info.uuid()
-            );
-            continue;
-        }
-
-        let tx = conn.transaction()?;
-        match update_elevation_data(&tx, elevation_hdl.as_ref(), file_info.id(), opts.overwrite) {
-            Ok(_) => {
-                tx.commit()?;
-                info!(
-                    "Successfully updated elevation for FIT file '{}'",
-                    file_info.uuid()
-                );
-            }
-            Err(e) => {
-                tx.rollback()?;
-                error!(
-                    "Could not import elevation data from the API for FIT file '{}'",
-                    file_info.uuid()
-                );
-                error!("{}", e);
-            }
-        }
+        update_file(&mut conn, elevation_hdl.as_ref(), &uuid, opts.overwrite)?;
     }
 
     // update missing elevation data in database
@@ -88,6 +60,91 @@ pub fn update_elevation_command(
         let tx = conn.transaction()?;
         update_elevation_data(&tx, elevation_hdl.as_ref(), None, false)?;
         tx.commit()?;
+    }
+
+    Ok(())
+}
+
+/// Print out the UUIDs of all files with missing elevation data
+fn list_missing(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
+    // queries
+    let rec_query = "select uuid from files where id in (
+        select distinct(file_id)
+        from record_messages
+        where position_lat is not null and
+            position_long is not null and
+            elevation is null
+    )";
+    let lap_query = "select uuid from files where id in (
+        select distinct(file_id)
+        from lap_messages
+        where start_position_lat is not null and
+            start_position_long is not null and
+            start_elevation is null
+    )";
+    let mut uuids = HashSet::new();
+    let mut stmt = conn.prepare(rec_query)?;
+    for uuid in stmt.query_map(params![], |row| row.get::<usize, String>(0))? {
+        uuids.insert(uuid?);
+    }
+    let mut stmt = conn.prepare(lap_query)?;
+    for uuid in stmt.query_map(params![], |row| row.get::<usize, String>(0))? {
+        uuids.insert(uuid?);
+    }
+
+    if uuids.is_empty() {
+        println!("No files have missing elevation data.");
+    } else {
+        println!(
+            "The following {:?} files have missing elevation data:",
+            uuids.len()
+        );
+        for uuid in uuids {
+            println!(" *\t{}", uuid);
+        }
+    }
+
+    Ok(())
+}
+
+/// Update the elevation data for a file, this suppresses all non-fatal errors and instead
+/// emits out logging messages for them.
+fn update_file<T: ElevationDataSource + ?Sized>(
+    conn: &mut Connection,
+    elevation_hdl: &T,
+    uuid: &str,
+    overwrite: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // locate file_id from uuid
+    let file_info = match find_file_by_uuid(&conn, &uuid) {
+        Ok(info) => info,
+        Err(e) => return Err(Box::new(e)),
+    };
+    if file_info.id().is_none() {
+        error!(
+            "File with UUID={} has no file_id cannot update elevation data.",
+            file_info.uuid()
+        );
+        return Ok(());
+    }
+
+    let tx = conn.transaction()?;
+    match update_elevation_data(&tx, elevation_hdl, file_info.id(), overwrite) {
+        Ok(_) => {
+            tx.commit()?;
+            info!(
+                "Successfully updated elevation for FIT file '{}'",
+                file_info.uuid()
+            );
+        }
+        Err(e) => {
+            tx.rollback()?;
+            error!(
+                "Could not import elevation data from the API for FIT file '{}'",
+                file_info.uuid()
+            );
+            error!("{}", e);
+        }
     }
 
     Ok(())
